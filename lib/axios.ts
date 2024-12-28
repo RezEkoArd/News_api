32ca2a3319c7af2407517496eb990af4d482ba36
Rezekoard <rezkyardianto1222@gmail.com>
2024-12-28 13:38:09 +0700

    feat: form login

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
-import { config } from "process"
 
-const baseUrl = process.env.NEXt_PUBLIC_API_URL
+
+const baseUrl = process.env.NEXT_PUBLIC_API_URL
 
 
 const axiosInstance = axios.create({
